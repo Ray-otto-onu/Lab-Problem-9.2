607f4253bb321b373491a5911827f8aeecf98df2
RAY OTTO <rjotto123@gmail.com>
2023-11-02 11:04:15 -0400

    Completed isSortedIncreasing

---
+++ P9-2/9.2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+bool isSortedIncreasing(int values[], int size);
+bool isSortedDecreasing(int values[], int size);
+bool hasAdjacentDuplicates(int values[], int size);
+bool hasDuplicates(int values[], int size);
+
+int main() {
+    int size = 6; 
+    int values[6];
+
+    for (int i = 0; i < size; i++) {
+        cout << "Enter integer #" << i << ": ";
+        cin >> values[i];
+    }
+
+    int increasing = isSortedIncreasing(values, size);
+    int decreasing = isSortedDecreasing(values, size);
+    int near_duplicates = hasAdjacentDuplicates(values, size);
+    int duplicates = hasDuplicates(values, size);
+
+    if (increasing)
+        cout << "The data are increasing." << endl;
+    else
+        cout << "The data are not increasing." << endl;
+
+    if (decreasing)
+        cout << "The data are decreasing." << endl;
+    else
+        cout << "The data are not decreasing." << endl;
+
+    if (near_duplicates)
+        cout << "The data has adjacent duplicates." << endl;
+    else
+        cout << "The data does not have adjacent duplicates." << endl;
+
+    if (duplicates)
+        cout << "The data has duplicates." << endl;
+    else
+        cout << "The data does not have duplicates." << endl;
+}
+
+bool isSortedIncreasing(int values[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (values[i] > values[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
