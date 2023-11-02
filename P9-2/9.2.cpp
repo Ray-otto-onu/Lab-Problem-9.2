a77a1f981316bcc261833b1846b28688af19d4e7
RAY OTTO <rjotto123@gmail.com>
2023-11-02 11:05:48 -0400

    Completed IsSortedDecreasing

--- P9-2/9.2.cpp
+++ P9-2/9.2.cpp
@@ -49,3 +49,14 @@ bool isSortedIncreasing(int values[], int size) {
     }
     return true;
 }
+
+bool isSortedDecreasing(int values[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (values[i] < values[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
